cc493046858032744c9f1b579d74aa573a29ae5a
vutienminh1919 <vutineminh1919@gmail.com>
2022-01-10 09:48:48 +0700

    fix admin

--- src/app/components/frontend/layout/admin/admin.component.ts
+++ src/app/components/frontend/layout/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../../service/auth.service";
 
 
 @Component({
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+
   }
 
 }
