39cfe288516b9242a277eb48f4807db88dc0ee28
nguyenthixuananh <nguyenthixuananh09061999@gmail.com>
2022-01-11 13:46:12 +0700

    create quiz

--- src/app/components/quiz/quiz-create/quiz-create.component.ts
+++ src/app/components/quiz/quiz-create/quiz-create.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {QuizService} from "../../../service/quiz.service";
 import {Router} from "@angular/router";
+import {QuestionService} from "../../../service/question/question.service";
 
 @Component({
   selector: 'app-quiz-create',
@@ -10,27 +11,59 @@ import {Router} from "@angular/router";
 })
 export class QuizCreateComponent implements OnInit {
 
+  questions: any[] = [];
+
   formAddQuiz: FormGroup = new FormGroup({
     name: new FormControl(),
     duration: new FormControl(),
-    published: new FormControl()
+    published: new FormControl(),
+    questions: new FormArray([]),
 
   })
 
   constructor(private quizService: QuizService,
-              private route: Router) { }
+              private questionService: QuestionService,
+              private route: Router) {
+  }
 
   ngOnInit(): void {
+    this.getQuestions();
   }
+
   submit() {
     const quiz = this.formAddQuiz.value;
+    console.log(quiz)
     this.quizService.addQuiz(quiz).subscribe(() => {
       this.formAddQuiz.reset();
       alert('Tạo thành công');
       this.route.navigate(["quizzes/list"])
-    } , error => {
+    }, error => {
       console.log(error)
     })
   }
 
+  getQuestions() {
+    this.questionService.getAll().subscribe(res => {
+      console.log(res);
+      this.questions = res;
+    })
+  }
+
+  // @ts-ignore
+  onCheckSubmit(event) {
+    const formArray: FormArray = this.formAddQuiz.get('questions') as FormArray;
+    if (event.target.checked) {
+      formArray.push(new FormControl(event.target.value));
+    } else {
+      let i: number = 0;
+      // @ts-ignore
+      formArray.controls.forEach((ctrl: FormControl) => {
+        if (ctrl.value == event.target.value) {
+          formArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
 }
