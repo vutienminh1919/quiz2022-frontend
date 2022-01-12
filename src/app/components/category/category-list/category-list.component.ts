c3abf0fa2c1a58ea47f763921460d77d17c70b8c
vutienminh1919 <vutineminh1919@gmail.com>
2022-01-12 14:28:35 +0700

    toast category done

--- src/app/components/category/category-list/category-list.component.ts
+++ src/app/components/category/category-list/category-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Category} from "../../../model/category";
 import {CategoryService} from "../../../service/category.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-category-list',
@@ -11,7 +12,9 @@ export class CategoryListComponent implements OnInit {
   categories: Category[]= [];
   p: number = 1;
   search: any;
-  constructor(private categoryService: CategoryService) { }
+
+  constructor(private categoryService: CategoryService,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.getAll();
@@ -32,6 +35,7 @@ export class CategoryListComponent implements OnInit {
         n => n.id !== category.id
       )
     })
+    this.toastr.success('Xóa thành công ', 'Thông báo')
 
   }
 }
