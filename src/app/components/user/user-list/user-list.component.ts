11dab7f808bbc3edc7bd77194e6d4a9fee7e9d69
vutienminh1919 <vutineminh1919@gmail.com>
2022-01-12 16:15:53 +0700

    pull

--- src/app/components/user/user-list/user-list.component.ts
+++ src/app/components/user/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../../service/user.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-user-list',
@@ -11,7 +12,8 @@ export class UserListComponent implements OnInit {
   p: number = 1;
   search: any;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,
+              private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
@@ -24,4 +26,19 @@ export class UserListComponent implements OnInit {
       console.log(this.users)
     });
   }
+
+  // delete(i: any) {
+  //   const user = this.users[i];
+  //
+  //   // @ts-ignore
+  //   this.userService.delete(user.id).subscribe(res => {
+  //     console.log(res)
+  //     this.users = this.users.filter(
+  //       n => n.id !== user.id
+  //     )
+  //   })
+  //
+  //
+  //   this.toastr.success('Xóa thành công ', 'Thông báo')
+  // }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -11,6 +11,9 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
   getAll(): Observable<any> {
-    return this.http.get<any>(API_URL + '/users');
+    return this.http.get<any>(API_URL + '/users', {headers:{'Authorization': `Bearer ${localStorage.getItem('token')}`}});
+  }
+  delete(id: any): Observable<any>{
+    return this.http.delete<any>(API_URL + `/users/${id}`, {headers:{'Authorization': `Bearer ${localStorage.getItem('token')}`}});
   }
 }
